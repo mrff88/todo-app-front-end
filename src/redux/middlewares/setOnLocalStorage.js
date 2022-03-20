a08d74c8d365ad8238b323d0d1c7b25288f39f0a
mrff88 <mrff88@gmail.com>
2022-03-20 10:04:47 -0500

    add setOnLocalStorage redux middleware to set token state after login or logout

---
+++ src/redux/middlewares/setOnLocalStorage.js
@@ -0,0 +1,22 @@
+const setOnLocalStorage = (store, action) => {
+  switch (action.type) {
+    case 'users/logOut':
+      localStorage.setItem(
+        'infoUser',
+        JSON.stringify({ token: store.getState().users.token })
+      );
+      break;
+    case 'login/fulfilled':
+      localStorage.setItem(
+        'infoUser',
+        JSON.stringify({ token: store.getState().users.token })
+      );
+      break;
+
+    default:
+      console.log(action.type);
+      break;
+  }
+};
+
+export default setOnLocalStorage;
