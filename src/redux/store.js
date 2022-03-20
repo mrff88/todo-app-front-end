6daf2b0a85c5053b2494264a23a07562f4918e5b
mrff88 <mrff88@gmail.com>
2022-03-20 10:13:05 -0500

    add user reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,27 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import taskSlice from './slices/taskSlice';
 import messageSlice from './slices/messageSlice';
 import userSlice from './slices/userSlice';
-
-const applicationState = 'applicationState';
-
-const localStorageMiddleware = (store) => {
-  return (next) => (action) => {
-    const result = next(action);
-    if (action.type.includes('tasks/')) {
-      localStorage.setItem(
-        applicationState,
-        JSON.stringify({ tasks: store.getState().tasks })
-      );
-    }
-    return result;
-  };
-};
-
-const loadStateFromLocalStorage = () => {
-  if (localStorage.getItem(applicationState) !== null) {
-    return JSON.parse(localStorage.getItem(applicationState));
-  }
-};
+import loadStateFromLocalStorage from './preloadedState/loadStateFromLocalStorage';
+import localStorageMiddleware from './middlewares/localStorageMiddleware';
 
 const store = configureStore({
   reducer: {
