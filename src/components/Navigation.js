4c816cd4a5fe83f8d46d812069357f272fb325bd
mrff88 <mrff88@gmail.com>
2022-03-20 10:15:02 -0500

    add navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,66 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logOut, selectToken } from '../redux/slices/userSlice';
+import parseJwt from '../util/parseJWT';
+
 const Navigation = () => {
+  const token = useSelector(selectToken);
+  const sesion = parseJwt(token);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cerrarSesion = () => {
+    dispatch(logOut());
+    navigate('/', { replace: true });
+  };
+
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="navbar sticky-top navbar-dark bg-dark navbar-expand-md">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          TODO APP
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarLinks"
+          aria-controls="navbarLinks"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse" id="navbarLinks">
+          <ul className="navbar-nav ms-auto align-items-center mb-2 mb-lg-0">
+            {sesion ? (
+              <li className="nav-item">
+                <span
+                  style={{ cursor: 'pointer' }}
+                  className="nav-link"
+                  onClick={cerrarSesion}
+                >
+                  Cerrar sesión
+                </span>
+              </li>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Regístrate
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/password-recovery">
+                    Recuperar contraseña
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
