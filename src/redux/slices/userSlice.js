d30737fdfce0566e6a5b31904344f9b135d7f834
mrff88 <mrff88@gmail.com>
2022-03-21 10:52:30 -0500

    add async thunks for user password reset

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -37,6 +37,52 @@ export const verifyAsync = createAsyncThunk(
   }
 );
 
+export const requestPasswordChangeLinkAsync = createAsyncThunk(
+  'users/requestPasswordChangeLink',
+  async (email, { rejectWithValue }) => {
+    try {
+      const { status, data } = await userCtlr.requestPasswordChangeLink(email);
+      if (status !== 200) throw new Error(data.message);
+      return data.message;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const verifyPasswordChangeLinkAsync = createAsyncThunk(
+  'users/verifyPasswordChangeLink',
+  async ({ id, token }, { rejectWithValue }) => {
+    try {
+      const { status, data } = await userCtlr.verifyPasswordChangeLink(
+        id,
+        token
+      );
+      if (status !== 200) throw new Error(data.message);
+      return data.message;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const changePasswordAsync = createAsyncThunk(
+  'userd/changePassword',
+  async ({ id, token, password }, { rejectWithValue }) => {
+    try {
+      const { status, data } = await userCtlr.changePassword(
+        id,
+        token,
+        password
+      );
+      if (status !== 200) throw new Error(data.message);
+      return data.message;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 export const loginAsync = createAsyncThunk('login', async (user) => {
   const { data } = await userCtlr.login(user);
   return data.token;
@@ -82,6 +128,57 @@ const userSlice = createSlice({
         state.isVerifying = false;
         state.verifyErrorMessage = message;
       })
+      .addCase(requestPasswordChangeLinkAsync.pending, (state) => {
+        state.isRequestingLink = true;
+        state.requestMessage = null;
+        state.requestErrorMessage = null;
+      })
+      .addCase(
+        requestPasswordChangeLinkAsync.fulfilled,
+        (state, { payload: message }) => {
+          state.isRequestingLink = false;
+          state.requestMessage = message;
+        }
+      )
+      .addCase(
+        requestPasswordChangeLinkAsync.rejected,
+        (state, { payload: message }) => {
+          state.isRequestingLink = false;
+          state.requestErrorMessage = message;
+        }
+      )
+      .addCase(verifyPasswordChangeLinkAsync.pending, (state) => {
+        state.isVerifyingPasswordChangeLink = true;
+        state.verifyPasswordChangeLinkMessage = null;
+        state.verifyPasswordChangeLinkErrorMessage = null;
+      })
+      .addCase(
+        verifyPasswordChangeLinkAsync.fulfilled,
+        (state, { payload: message }) => {
+          state.isVerifyingPasswordChangeLink = false;
+          state.verifyPasswordChangeLinkMessage = message;
+        }
+      )
+      .addCase(
+        verifyPasswordChangeLinkAsync.rejected,
+        (state, { payload: message }) => {
+          state.isVerifyingPasswordChangeLink = false;
+          state.verifyPasswordChangeLinkErrorMessage = message;
+        }
+      )
+      .addCase(changePasswordAsync.pending, (state) => {
+        state.isChangingPassword = true;
+        state.changePasswordMessage = null;
+        state.changePasswordErrorMessage = null;
+      })
+      .addCase(changePasswordAsync.fulfilled, (state, { payload: message }) => {
+        state.isChangingPassword = false;
+        state.changePasswordMessage = message;
+      })
+      .addCase(changePasswordAsync.rejected, (state, { payload: message }) => {
+        state.isChangingPassword = false;
+        state.changePasswordErrorMessage = message;
+      })
       .addCase(loginAsync.pending, (state) => {
         state.isLoginIn = true;
       })
@@ -98,12 +195,28 @@ export const { logOut, setToken } = userSlice.actions;
 // selectors
 export const selectUsers = (state) => state.users.userList;
 export const selectToken = (state) => state.users.token;
+export const selectIsLoading = (state) => state.users.isLoading;
 export const selectIsCreating = (state) => state.users.isCreating;
 export const selectIsVerifying = (state) => state.users.isVerifying;
+export const selectIsRequestingLink = (state) => state.users.isRequestingLink;
+export const selectIsVerifyingPasswordChangeLink = (state) =>
+  state.users.isVerifyingPasswordChangeLink;
+export const selectIsChangingPassword = (state) =>
+  state.users.isChangingPassword;
 export const selectVerifyMessage = (state) => state.users.verifyMessage;
+export const selectRequestMessage = (state) => state.users.requestMessage;
+export const selectVerifyPasswordChangeLinkMessage = (state) =>
+  state.users.verifyPasswordChangeLinkMessage;
+export const selectChangePasswordMessage = (state) =>
+  state.users.changePasswordMessage;
 export const selectVerifyErrorMessage = (state) =>
   state.users.verifyErrorMessage;
-export const selectIsLoading = (state) => state.users.isLoading;
+export const selectRequestErrorMessage = (state) =>
+  state.users.requestErrorMessage;
+export const selectVerifyPasswordChangeLinkErrorMessage = (state) =>
+  state.users.verifyPasswordChangeLinkErrorMessage;
+export const selectChangePasswordErrorMessage = (state) =>
+  state.users.changePasswordErrorMessage;
 
 // thunks
 
